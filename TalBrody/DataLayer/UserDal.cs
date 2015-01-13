ec0afa620ffe9909f46c14969601491aa137f70b
Ron Gross <ron.gross@gmail.com>
2015-01-13 17:38:18 +0200

    Working on user registration. Form works, thanks Ziv

--- TalBrody/DataLayer/UserDal.cs
+++ TalBrody/DataLayer/UserDal.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Data.OleDb;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Data.SqlServerCe;
+using fblogin.Entity;
 using log4net;
 
 namespace fblogin.DataLayer
@@ -29,14 +31,14 @@ namespace fblogin.DataLayer
 			int Result = 0;
 			try
 			{
-				using (SqlCeConnection myConnection = GetPortalConnection())
+				using (var myConnection = GetPortalConnection())
 				{
-					SqlCeCommand myCommand = new SqlCeCommand("select count(*) from Follwers where ProjectId = " + ProjectId, myConnection);
+                    // TODO Fix SQL Injection
+					var myCommand = new SqlCeCommand("select count(*) from Follwers where ProjectId = " + ProjectId, myConnection);
 					//myCommand.Parameters.Add("@ProjectId", SqlDbType.Int).Value = ProjectId;
 					myCommand.CommandType = CommandType.Text;
 					myConnection.Open();
 					Result = int.Parse(myCommand.ExecuteScalar().ToString());					
-					myConnection.Close();
 				//	Result = int.Parse(myCommand.Parameters["@ProjectId"].ToString());
 
 				}
@@ -47,5 +49,32 @@ namespace fblogin.DataLayer
 			}
 			return Result;
 		}
+
+	    public User FindUserByEmail(string email)
+	    {
+            using (var myConnection = GetPortalConnection())
+            {
+                // TODO Fix SQL Injection
+//                var myCommand = new SqlCeCommand("select Id, Email from Users where Email = " + email, myConnection);
+                var myCommand = new SqlCeCommand("select Id, Email from Users where Email = @Email", myConnection);
+                myCommand.Parameters.Add("@Email", SqlDbType.NVarChar).Value = email;
+
+                myCommand.CommandType = CommandType.Text;
+
+                myConnection.Open();
+                var reader = myCommand.ExecuteReader();
+                if (!reader.Read())
+                {
+                    return null;
+                }
+
+                var user = new User
+                {
+                    Id = (int) reader[0], 
+                    Email = (string) reader[1]
+                };
+                return user;
+            }
+	    }
 	}
 }
