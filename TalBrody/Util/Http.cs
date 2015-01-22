33c4a3506306e3ba983cbfe9bf61bd2b9e473caa
Ron Gross <ron.gross@gmail.com>
2015-01-22 19:05:15 +0200

    Add Mandrill Mailer

---
+++ TalBrody/Util/Http.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace TalBrody.Util
+{
+    public static class Http
+    {
+        public static byte[] Post(string uri, NameValueCollection pairs)
+        {
+            using (var client = new WebClient())
+            {
+                return client.UploadValues(uri, pairs);
+            }
+        }
+    }
+}
