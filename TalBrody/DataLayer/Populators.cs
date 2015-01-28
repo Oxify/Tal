b411c6edf381e495bc7a697ed63f46fa16b1c67d
zivTzoran <zivverb@hotmail.com>
2015-01-28 23:43:16 +0200

    add dynamic to populate

--- TalBrody/DataLayer/Populators.cs
+++ TalBrody/DataLayer/Populators.cs
@@ -7,30 +7,18 @@ namespace TalBrody.DataLayer
 {
 	public class Populators
 	{
+		internal static Follower Populate_Follower(dynamic dr)
+		{
+			Follower Consept = new Follower();
+			Consept.ContactEmail = dr.GetValue<string>("ContactEmail");
+			Consept.DateCreated = dr.GetValue<DateTime>("DateCreated");
+			Consept.ConseptId = dr.GetValue<int>("ConseptId");
+			Consept.count = dr.GetValue<int>("count");
+			Consept.ContactId = dr.GetValue<int>("ContactId");
+			return Consept;
+		}
 		
-		internal static Follower Populate_Follower(SqlDataReader dr)
-		{
-			Follower Consept = new Follower();
-			Consept.ContactEmail = dr.GetValue<string>("ContactEmail");
-			Consept.DateCreated = dr.GetValue<DateTime>("DateCreated");
-			Consept.ConseptId = dr.GetValue<int>("ConseptId");
-			Consept.count = dr.GetValue<int>("count");
-			Consept.ContactId = dr.GetValue<int>("ContactId");
-			return Consept;
-		}
-
-		internal static Follower Populate_Follower(SqlCeDataReader dr)
-		{
-			Follower Consept = new Follower();
-			Consept.ContactEmail = dr.GetValue<string>("ContactEmail");
-			Consept.DateCreated = dr.GetValue<DateTime>("DateCreated");
-			Consept.ConseptId = dr.GetValue<int>("ConseptId");
-			Consept.count = dr.GetValue<int>("count");
-			Consept.ContactId = dr.GetValue<int>("ContactId");
-			return Consept;
-		}
-
-		internal static Perks Populate_Perks(SqlCeDataReader dr)
+		internal static Perks Populate_Perks(dynamic dr)
 		{
 			Perks Perks = new Perks();
 			Perks.Cost = dr.GetValue<int>("Cost");
@@ -42,19 +30,7 @@ namespace TalBrody.DataLayer
 			return Perks;
 		}
 
-		internal static Perks Populate_Perks(SqlDataReader dr)
-		{
-			Perks Perks = new Perks();
-			Perks.Cost = dr.GetValue<int>("Cost");
-			Perks.Description = dr.GetValue<string>("Description");
-			Perks.PerkId = dr.GetValue<int>("PerkId");
-			Perks.ProjectId = dr.GetValue<int>("ProjectId");
-			Perks.ShowOrder = dr.GetValue<int>("ShowOrder");
-			Perks.Title = dr.GetValue<string>("Title");
-			return Perks;
-		}
-
-		internal static Project Populate_Project(SqlCeDataReader dr)
+		internal static Project Populate_Project(dynamic dr)
 		{
 			Project Project = new Project();
 			Project.Description = dr.GetValue<string>("Description");
@@ -65,29 +41,8 @@ namespace TalBrody.DataLayer
 			Project.ShortName = dr.GetValue<string>("ShortName");
 			return Project;
 		}
-
-		internal static Project Populate_Project(SqlDataReader dr)
-		{
-			Project Project = new Project();
-			Project.Description = dr.GetValue<string>("Description");
-			Project.DisplayName = dr.GetValue<string>("DisplayName");
-			Project.id = dr.GetValue<int>("id");
-			Project.LinkUrl = dr.GetValue<string>("LinkUrl");
-			Project.MovieUrl = dr.GetValue<string>("MovieUrl");
-			Project.ShortName = dr.GetValue<string>("ShortName");
-			return Project;
-		}
-
-	    internal static Param Populate_Params(SqlDataReader dr)
-	    {
-            Param param = new Param();
-	        param.Name = dr.GetValue<string>("Name");
-	        param.Value = dr.GetValue<string>("Value");
-	        param.ValueInt = dr.GetInt32(dr.GetOrdinal("ValueInt"));
-	        
-            return param;
-	    }
-        internal static Param Populate_Params(SqlCeDataReader dr)
+			   
+        internal static Param Populate_Params(dynamic dr)
         {
             Param param = new Param();
             param.Name = dr.GetValue<string>("Name");
@@ -97,7 +52,7 @@ namespace TalBrody.DataLayer
             return param;
         }
 
-		internal static ProjectDetails Populate_projectDetails(SqlDataReader dr)
+		internal static ProjectDetails Populate_projectDetails(dynamic dr)
 		{
 			ProjectDetails ProjectDetails = new ProjectDetails();
 			ProjectDetails.FieldId = dr.GetValue<int>("FieldId");
