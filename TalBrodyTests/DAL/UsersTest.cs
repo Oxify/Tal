a66015891b29a09b35f2e0c472ad372d7f39d771
Ron Gross <ron.gross@gmail.com>
2015-02-09 12:26:09 +0200

    Random email per test

--- TalBrodyTests/DAL/UsersTest.cs
+++ TalBrodyTests/DAL/UsersTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TalBrody.DataLayer;
 using TalBrody.Logic;
+using System.Text;
 
 namespace TalBrodyTests.DAL
 {
@@ -11,19 +12,38 @@ namespace TalBrodyTests.DAL
         [TestMethod]
         public void FindUserByEmailTest()
         {
-            var name = "Muhamad Ali";
+            var email = RandomString(10) + "@mail.com";
             var password = "12344321";
 
             var users = new UserDal();
             
             // Check user does not exist
-            Assert.IsFalse(Users.CheckUserPassword(name, password));
+            Assert.IsFalse(Users.CheckUserPassword(email, password));
 
             // Create it
-            Users.CreateUser(name, password);
+            Users.CreateUser(email, password);
 
             // Check it does exist
-            Assert.IsTrue(Users.CheckUserPassword(name, password));
+            Assert.IsTrue(Users.CheckUserPassword(email, password));
         }
+
+        #region Helpers
+
+        private static Random random = new Random((int)DateTime.Now.Ticks);//thanks to McAden
+
+        private string RandomString(int size)
+        {
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            return builder.ToString();
+        }
+
+        #endregion
     }
 }
