ad111f2e8455d1d10025f9d1ff6d0b62f33dd15a
Ron Gross <ron.gross@gmail.com>
2015-02-10 19:10:16 +0200

    Create Share page

---
+++ TalBrody/Share.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TalBrody
+{
+    public partial class Share : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ShareUrl = "http://tal.apphb.com/?r=mfly107JAncl";
+            FacebookShareUrl = "https://www.facebook.com/sharer/sharer.php?app_id=1423139441310101&u=" + HttpUtility.UrlEncode(ShareUrl) + "&display=popup&ref=plugin";
+            TwitterShareUrl = "https://twitter.com/share?url=" + HttpUtility.UrlEncode(ShareUrl);
+        }
+
+        public string ShareUrl;
+        public string FacebookShareUrl;
+        public string TwitterShareUrl;
+    }
+}
