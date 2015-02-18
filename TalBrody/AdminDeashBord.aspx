433e5def78913c179fde73951f31f030ab475616
zivTzoran <zivverb@hotmail.com>
2015-02-18 02:32:38 +0200

    semi finish add permission to user by project manager

---
+++ TalBrody/AdminDeashBord.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Oxify.Master" CodeBehind="AdminDeashBord.aspx.cs" Inherits="TalBrody.AdminDeashBord" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+
+</asp:Content>
---
+++ TalBrody/EditProject.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TalBrody
+{
+    public partial class EditProject : System.Web.UI.Page
+    {
+        public int ProjectId = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                InitParam();
+            }
+        }
+
+        private void InitParam()
+        {
+            ProjectId = int.Parse(Request["ProjectId"]);
+        }
+
+        private void PopulateFollwers()
+        {
+
+
+        }
+    }
+}
