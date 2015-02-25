e9ddd8a6c12a79d22396a0d921042b8b1763cf7b
Ortal Raz <ortalr@gmail.com>
2015-02-25 05:29:39 +0200

    And more progress on project.aspx

--- TalBrody/JavaScript/Project.js
+++ TalBrody/JavaScript/Project.js
@@ -4,7 +4,12 @@ $(document).ready(function () {
     for (var i = 0; i < elements.length; i++) {
         elements[i].onclick = ScrollToTarget;
     }
-
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        var elem = document.getElementsByClassName('whatsapp-box');
+        for ( i = 0; i < elements.length; i++) {
+            elements[i].style.display = 'inline-block';
+        }
+    }
 });
 
 function ScrollToTarget() {
