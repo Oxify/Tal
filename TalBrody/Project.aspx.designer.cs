0ff6dcfd52950551ef6d29c7ceeabe40b0b5793c
Ortal Raz <ortalr@gmail.com>
2015-03-01 15:49:50 +0200

    fix build

--- TalBrody/Project.aspx.designer.cs
+++ TalBrody/Project.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace TalBrody {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl projectpage;
         
         /// <summary>
+        /// firstpage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl firstpage;
+        
+        /// <summary>
         /// DiscountDiv control.
         /// </summary>
         /// <remarks>
